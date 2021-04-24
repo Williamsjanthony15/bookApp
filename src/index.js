740b4095e9d2e8627dd45489c544d24b79b1a491
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-04-24 14:11:57 -0500

    Initial deploy with Auth0 provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider 
+    domain="dev-fb0m2j2w.us.auth0.com"
+    clientId="9JyXbJ5IFKnPJ00JY9i73yY3mTWFDMdv"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+    document.getElementById("root")
 );
