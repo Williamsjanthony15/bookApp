9cac296693c8a09c620bf2d28e4be2a63113e638
santorsm <matt.santorsola@gmail.com>
2021-05-03 22:13:13 -0700

    card 1

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { withAuth0 } from '@auth0/auth0-react';
+import IsLoadingAndError from './IsLoadingAndError';
 
 class BookFormModal extends React.Component {
   constructor(props) {
@@ -10,8 +13,10 @@ class BookFormModal extends React.Component {
       name: '',
       description: '',
       status: '',
+      err: false,
     };
   }
+
   handleBookNameSubmit = (e) => {
     this.setState({ name: e.target.value })
   }
@@ -21,13 +26,27 @@ class BookFormModal extends React.Component {
   handleBookDescriptionSubmit = (e) => {
     this.setState({ description: e.target.value })
   }
-  handleFormSubmit = (e) => {
-    e.preventDefault();
-    
+  handleFormSubmit = async() => {
+    try{
+    const email = this.props.auth0.user.email;
+
+    axios.post(process.env.REACT_APP_BACKEND_URL+'/books',{
+      email: email,
+      name: this.state.name,
+      description: this.state.description,
+      status: this.state.status,
+    })
+  } catch (error) {
+    this.setState ({
+      error:true,
+      errorMessage: error.message,
+    })
+  }
   }
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.props.hideModal}>
+      // {this.state.error ? < IsLoadingAndError errorMessage={this.state.errorMessage} /> : ''}
+      <Modal show={this.props.modalSeen} onHide={this.props.close}>
         <Modal.Header closeButton>
           <Modal.Title>Add a book to your collection</Modal.Title>
         </Modal.Header>
@@ -49,18 +68,18 @@ class BookFormModal extends React.Component {
                 <option>Not Recommened</option>
               </Form.Control>
               <br />
-              <Button variant="success" size="lg" onClick={this.props.open}>
+              <Button variant="success" size="lg" onClick={this.handleFormSubmit}>
                 Submit!
               </Button>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-        <Button variant="secondary" onClick={this.props.hideModal}>Close</Button>
+        <Button variant="secondary" onClick={this.props.close}>Close</Button>
         </Modal.Footer>
       </Modal >
     )
   }
 }
 
-export default BookFormModal;
+export default withAuth0(BookFormModal);
